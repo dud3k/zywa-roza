58f97167145b7d3081c463314ba5539be3ffb62f
dud3k <marcin-dudek@o2.pl>
2026-06-18 13:28:26 +0200

    feat(core): barrel index.ts (publiczne API core)

---
+++ src/core/index.ts
@@ -0,0 +1,8 @@
+export { TAJEMNICE, ZESTAWY, KOLOR_VAR, RZYM } from './mysteries';
+export type { Zestaw, Tajemnica } from './mysteries';
+
+export { indeksTajemnicy, tajemnica } from './rotation';
+export type { Kotwica, TajemnicaWidok } from './rotation';
+
+export { displayNamesDla } from './displayName';
+export type { OsobaImie } from './displayName';
